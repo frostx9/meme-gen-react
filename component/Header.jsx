6bf97e2f8550e07ab1de85c8818c0b8a32439f78
Saumya Som <saumyasom92@gmail.com>
2023-02-01 23:41:42 +0530

    Intial Commit

---
+++ component/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <img src="/troll-face.png" alt="" />
+        <h2>Meme Generater</h2>
+      </header>
+    </div>
+  )
+}
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../component/Header'
+
+
+export default function App() {
+
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+
